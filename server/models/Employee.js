a41eadfdc250c64554a8c5a70b614d700aebc218
Samith-hegde <78680288+Samith-hegde@users.noreply.github.com>
2024-07-16 18:34:47 +0530

    Update Employee.js

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,1 +1,34 @@
+const mongoose = require('mongoose');
+const formatDate = require('../utils/dateformatter');
 
+const EmployeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  empid: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  department: {
+    type: String,
+    required: true
+  },
+  dob: {
+    type: String,
+    format: formatDate,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+});
+
+module.exports = mongoose.model('Employee', EmployeeSchema);
